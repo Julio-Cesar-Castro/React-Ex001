9ddf784e625de897a2218401ae3e4e7f98742a32
Juliocastro240902 <Juliocastro.contato@gmai.com>
2022-07-09 19:19:26 -0300

    React Exercise 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Maca from "./Maca.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Exercise extends Component {
+  state = {
+    nome: "Julio",
+    idade: 20,
+    comidaFavorita: "Lamen",
+    musicas: ["Those Eyes", "Glimpse of Us", "Baby Came Home 2"],
+  };
+
+  render() {
+    return (
+      <div>
+        <h1> {this.state.nome} </h1>
+        <h2> {this.state.idade} </h2>
+        <h3> {this.state.comidaFavorita} </h3>
+        <ul>
+          <li> {this.state.musicas[0]} </li>
+          <li> {this.state.musicas[1]} </li>
+          <li> {this.state.musicas[2]} </li>
+        </ul>
+        <img src={Maca} alt="Maça"></img>
+      </div>
+    );
+  }
 }
-
-export default App;
